2d3bf9ff8d9dc53295e5d759c65c76b1c6949a0d
Gary Huong <garyhuong@gmail.com>
2021-03-12 16:48:07 -0500

    Auto-focus text input on render and on user typing

--- src/components/NewMessageForm.js
+++ src/components/NewMessageForm.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+
+import { useStartTyping } from "react-use";
 
 // Bootstrap CSS
 import Button from "react-bootstrap/Button";
@@ -11,21 +13,25 @@ const NewMessageForm = ({
   handleNewMessageChange,
   className,
 }) => {
+  const messageRef = useRef(null); // reference to a component
+
+  // Auto-focus message input on render
+  useEffect(() => messageRef.current.focus(), []);
+
+  useStartTyping(() => messageRef.current.focus());
+
   return (
     <div className={className}>
-      <Form
-        onSubmit={handleSendMessage}
-        inline
-      >
-        <Form.Label srOnly>
-          Type Your Message
-        </Form.Label>
+      <Form onSubmit={handleSendMessage} autocomplete="off" inline>
+        <Form.Label srOnly>Type Your Message</Form.Label>
         <Form.Control
           id="message"
           className={`${styles.new_message_input}`}
           placeholder="> Type your message..."
           value={newMessage}
           onChange={handleNewMessageChange}
+          ref={messageRef}
+          autoFocus
         />
         <Button variant="primary" className={`${styles.btn_send_message}`}>
           Send
