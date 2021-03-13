ce13c921f777570a711b6dafba93ddb09dce6379
Gary Huong <garyhuong@gmail.com>
2021-03-12 19:07:49 -0500

    Add commented-out functionality for pinning latest message to the top

--- src/components/MessagesPanel.js
+++ src/components/MessagesPanel.js
@@ -3,21 +3,33 @@ import React, { useEffect, useRef } from "react";
 import styles from "./css/MessagesPanel.module.css";
 
 const MessagesPanel = ({ messages, className }) => {
-  const bottomOfMessagesRef = useRef(null); // bottom of messages
+  const latestRef = useRef(null); // ref to latest message
 
   useEffect(() => {
-    // scroll down to latest message when received
-    bottomOfMessagesRef.current.scrollIntoView({ behavior: "smooth" });
+    if (latestRef.current) {
+      // scroll down to latest message when received
+      latestRef.current.scrollIntoView({ behavior: "smooth" });
+    }
   }, [messages]);
+
+  // Returns html elements for a list of messages
+  const listOfMessages = (messages) => {
+    return messages.map((message, i) => {
+      // To scroll to the top of latest message instead
+      const ref = null; // i === messages.length - 1 ? latestRef : null;
+      return (
+        <li key={i} ref={ref}>
+          {message.body}
+        </li>
+      );
+    });
+  };
 
   return (
     <div className={`${styles.messages_panel} ${className}`}>
-      <ol className={styles.messages_list}>
-        {messages.map((message, i) => (
-          <li key={i /* TODO: idx as key = anti-pattern */}>{message.body}</li>
-        ))}
-      </ol>
-      <div ref={bottomOfMessagesRef} />
+      <ol className={styles.messages_list}>{listOfMessages(messages)}</ol>
+      <div ref={latestRef} />
+      {/* <div className={styles.space_under_messages}></div> */}
     </div>
   );
 };
